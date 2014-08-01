3f140b9e4c6796b003a5c8c829afa60010c01089
jmadden85 <jmadden85@gmail.com>
2014-08-01 13:20:12 -0400

    updated bower and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var bower = require('gulp-bower');
 //Set default destination
 gulp.task('bower', function () {
   return bower()
-    .pipe(gulp.dest('lib/'))
+    .pipe(gulp.dest('lib/dependencies'))
 });
 
 gulp.task('default', ['bower']);
