fc44ab2ae332a1900c51ded37dffd617a173b2d6
Joe Madden <Joe@beardbro.com>
2014-08-01 21:43:38 -0400

    more setup stuff, adding express and changes to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var bower = require('gulp-bower');
 //Set default destination
 gulp.task('bower', function () {
   return bower()
-    .pipe(gulp.dest('lib/dependencies'))
+    .pipe(gulp.dest('public/lib/dependencies'))
 });
 
 gulp.task('default', ['bower']);
