ad97df2b72bb4886130d0d84aa1abce13f80a67c
jmadden85 <jmadden85@gmail.com>
2014-08-04 18:30:13 -0400

    comments

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ var fs = require('fs');
 var _ = require('underscore');
 
 var getDirectories = function (path, callback) {
-  var path = path || '../';
+  //Set a default path
+  var path = path || process.cwd;
+  //Get directory contents
   fs.readdir(path, function (err, files) {
     if (err) {
       console.log(err);
     } else {
+      //create a variable for filtering out only directories & one for callback tracking
       var directories = [];
       var time =files.length;
-
+      //Loop through files and get stats object
       files.forEach(function(file, index, array) {
         fs.stat(path + file, function (err, stats){
           if (err) {
             console.log(err);
           } else {
+            //check if it's directory and push to directories array
             stats.isDirectory() ? directories.push(file) : false;
             if (time-- === 1) {
+              //Done iterating, return the data
               callback(directories);
             }
           }
