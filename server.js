5f87d619a63c524b04a454fc8626b3b0811b6c11
jmadden85 <jmadden85@gmail.com>
2014-08-04 18:28:34 -0400

    file system function for getting list of directories in a directory

--- server.js
+++ server.js
@@ -2,21 +2,32 @@
 var fs = require('fs');
 var _ = require('underscore');
 
-fs.readdir('../', function (err, files) {
-  var directories;
-  if (err) {
-    console.log(err);
-  } else {
-    var directories =files.filter(function(file) {
-      var isDirectory = fs.stat('../' + file,
-        function (err, stats) {
+var getDirectories = function (path, callback) {
+  var path = path || '../';
+  fs.readdir(path, function (err, files) {
+    if (err) {
+      console.log(err);
+    } else {
+      var directories = [];
+      var time =files.length;
+
+      files.forEach(function(file, index, array) {
+        fs.stat(path + file, function (err, stats){
           if (err) {
             console.log(err);
           } else {
-            return stats.isDirectory();
+            stats.isDirectory() ? directories.push(file) : false;
+            if (time-- === 1) {
+              callback(directories);
+            }
           }
-        }
-      );
-    });
-  }
+        });
+      });
+    }
+  });
+};
+
+
+getDirectories('../', function (data) {
+  console.log(data);
 });
