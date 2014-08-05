942d07e10a807cf6e246777538f7c1b330b884c7
Joe Madden <Joe@beardbro.com>
2014-08-05 01:22:20 -0400

    adding methods for getting file contents

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 'use strict';
 var fs = require('fs');
-var _ = require('underscore');
 
-var getDirectories = function (path, callback) {
-  //Set a default path
-  var path = path || process.cwd;
-  //Get directory contents
-  fs.readdir(path, function (err, files) {
-    if (err) {
-      console.log(err);
-    } else {
-      //create a variable for filtering out only directories & one for callback tracking
-      var directories = [];
-      var time =files.length;
-      //Loop through files and get stats object
-      files.forEach(function(file, index, array) {
-        fs.stat(path + file, function (err, stats){
-          if (err) {
-            console.log(err);
-          } else {
-            //check if it's directory and push to directories array
-            stats.isDirectory() ? directories.push(file) : false;
-            if (time-- === 1) {
-              //Done iterating, return the data
-              callback(directories);
-            }
-          }
-        });
+var files = {
+  //gets contents of a directory
+  getContents: function (path, callback) {
+    fs.readdir(path, function (err, files) {
+      if (err) throw err;
+      callback(files);
+    });
+  },
+  //gets directories or files in a directory
+  getDirectoriesOrFiles: function (options, callback) {
+    var directories = [];
+    var files = [];
+    var loops = options.files.length - 1;
+    options.files.forEach(function (file, index) {
+      fs.stat(options.path + file, function (err, stats) {
+        if (err) throw err;
+        stats.isDirectory() ? directories.push(file) : files.push(file);
+        if (loops === index) {
+          options.type === 'directories' ? callback(directories) : callback(files);
+        }
       });
-    }
-  });
+    });
+  }
 };
 
-
-getDirectories('../', function (data) {
-  console.log(data);
+files.getContents('../', function (data) {
+  files.getDirectoriesOrFiles({
+    path: '../',
+    files: data,
+    type: 'directories'
+  }, function (data) {
+    console.log(data);
+  });
 });
